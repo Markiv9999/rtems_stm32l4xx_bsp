ccfe173b936b993777549ae593c2cf9e8fcf3a94
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2023-12-20 21:52:45 +0000

    forgot to add the irq start aswell -.-

--- start/bspstart.c
+++ start/bspstart.c
@@ -8,8 +8,5 @@ uint32_t stm32l4_systick_frequency(void) { return SystemCoreClock; }
 
 void bsp_start(void) {
   // init_main_osc();
-
-  // stm32f4_gpio_set_config_array(&stm32f4_start_config_gpio[0]);
-
-  // bsp_interrupt_initialize();
+  bsp_interrupt_initialize();
 }
