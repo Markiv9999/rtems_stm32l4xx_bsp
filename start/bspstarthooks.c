30e3f1c295b440c9d24db6eec3ac345da68ff677
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2023-12-21 23:02:53 +0000

    tentative disabling bsp_start_clear_bss

--- start/bspstarthooks.c
+++ start/bspstarthooks.c
@@ -31,5 +31,6 @@ void BSP_START_TEXT_SECTION bsp_start_hook_0(void) {}
 
 void BSP_START_TEXT_SECTION bsp_start_hook_1(void) {
   bsp_start_copy_sections();
-  bsp_start_clear_bss();
+  // HACK: bsp_start_clear_bss temporarly disabled
+  // bsp_start_clear_bss(); TODO: put back (eventually)
 }
