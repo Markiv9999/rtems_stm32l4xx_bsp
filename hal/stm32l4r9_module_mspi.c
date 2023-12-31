c191f717b65cd01d2dcd6be37796fa92fbfc7b63
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2023-12-31 09:49:09 +0000

    headers fix to get compile_commands.jsom

--- hal/stm32l4r9_module_mspi.c
+++ hal/stm32l4r9_module_mspi.c
@@ -1,43 +1,45 @@
-#include <stdint.h>
-#include <stdlib.h>
-//#include "cmsis_gcc.h"
-#include "stm32l476xx.h"
+#include "stm32l4r9_module_mspi.h"
 #include "error_codes.h"
+#include "stm32l476xx.h"
 #include "typedefs.h"
-#include "module_mspi.h"
+
+#include <stdlib.h>
 
 /**
-* --------------------------------------------------------------------------- *
-*       INTERFACE SPECIFIC METHODS
-* --------------------------------------------------------------------------- *
-*/
+ * --------------------------------------------------------------------------- *
+ *       INTERFACE SPECIFIC METHODS
+ * --------------------------------------------------------------------------- *
+ */
 
 int mspi_init(void) {
   /* this procedure satisfy the following functions:
-  * - enabling of clock for peripheral and relative bus
-  * - configuring GPIO as required
-  *   the function MUST be configured in accordance with the system configuration 
-  */
+   * - enabling of clock for peripheral and relative bus
+   * - configuring GPIO as required
+   *   the function MUST be configured in accordance with the system
+   * configuration
+   */
 
-  /* Enables the clock for the gpios, sets to alternate function and configures the alternate function */
+  /* Enables the clock for the gpios, sets to alternate function and configures
+   * the alternate function */
   mspi_gpio_cfg();
   /* Enables the multi-spi clock, enables the multispi-dma */
   mspi_sys_cfg();
-  /* Configure device specific mspi settings, like clock prescaler, device size */
+  /* Configure device specific mspi settings, like clock prescaler, device size
+   */
   mspi_dev_cfg();
 
   /* PENDING
-  * Currently no checks are performed to assure the successful
-  * configuration of the system. This is considered a pending feauture
-  */
+   * Currently no checks are performed to assure the successful
+   * configuration of the system. This is considered a pending feauture
+   */
   return EXIT_UNDEFINED;
 }
-void mspi_gpio_cfg(void){
-  //configure alternate function for CLK and CS
-  GPIOB->AFR[1] &=  ~GPIO_AFRH_AFSEL10_Msk;
-  GPIOB->AFR[1] &=  ~GPIO_AFRH_AFSEL11_Msk;
-  GPIOB->AFR[1] |=  0b1010 << GPIO_AFRH_AFSEL10_Pos;
-  GPIOB->AFR[1] |=  0b1010 << GPIO_AFRH_AFSEL11_Pos;
+void mspi_gpio_cfg(void) {
+  // configure alternate function for CLK and CS
+  GPIOB->AFR[1] &= ~GPIO_AFRH_AFSEL10_Msk;
+  GPIOB->AFR[1] &= ~GPIO_AFRH_AFSEL11_Msk;
+  GPIOB->AFR[1] |= 0b1010 << GPIO_AFRH_AFSEL10_Pos;
+  GPIOB->AFR[1] |= 0b1010 << GPIO_AFRH_AFSEL11_Pos;
 
   GPIOB->MODER &= ~GPIO_MODER_MODE10_Msk;
   GPIOB->MODER &= ~GPIO_MODER_MODE11_Msk;
@@ -56,16 +58,15 @@ void mspi_gpio_cfg(void){
   GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD10_Msk;
   GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD11_Msk;
 
+  GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL6_Msk;
+  GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL7_Msk;
+  GPIOA->AFR[0] |= 0b1010 << GPIO_AFRL_AFSEL6_Pos;
+  GPIOA->AFR[0] |= 0b1010 << GPIO_AFRL_AFSEL7_Pos;
 
-  GPIOA->AFR[0] &=  ~GPIO_AFRL_AFSEL6_Msk;
-  GPIOA->AFR[0] &=  ~GPIO_AFRL_AFSEL7_Msk;
-  GPIOA->AFR[0] |=  0b1010 << GPIO_AFRL_AFSEL6_Pos;
-  GPIOA->AFR[0] |=  0b1010 << GPIO_AFRL_AFSEL7_Pos;
-
-  GPIOB->AFR[0] &=  ~GPIO_AFRL_AFSEL0_Msk;
-  GPIOB->AFR[0] &=  ~GPIO_AFRL_AFSEL1_Msk;
-  GPIOB->AFR[0] |=  0b1010 << GPIO_AFRL_AFSEL0_Pos;
-  GPIOB->AFR[0] |=  0b1010 << GPIO_AFRL_AFSEL1_Pos;
+  GPIOB->AFR[0] &= ~GPIO_AFRL_AFSEL0_Msk;
+  GPIOB->AFR[0] &= ~GPIO_AFRL_AFSEL1_Msk;
+  GPIOB->AFR[0] |= 0b1010 << GPIO_AFRL_AFSEL0_Pos;
+  GPIOB->AFR[0] |= 0b1010 << GPIO_AFRL_AFSEL1_Pos;
 
   GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
   GPIOA->MODER &= ~GPIO_MODER_MODE7_Msk;
@@ -103,7 +104,7 @@ void mspi_sys_cfg(void) {
   /* Enable the Quad-SPI interface clock */
   RCC->AHB3ENR |= RCC_AHB3ENR_QSPIEN;
   /* Reset Quad-SPI peripheral */
-  RCC->AHB3RSTR |= (RCC_AHB3RSTR_QSPIRST); /* Reset */
+  RCC->AHB3RSTR |= (RCC_AHB3RSTR_QSPIRST);  /* Reset */
   RCC->AHB3RSTR &= ~(RCC_AHB3RSTR_QSPIRST); /* Release reset */
   /* Enable Quad-SPI DMA */
   QUADSPI->CR |= QUADSPI_CR_DMAEN;
@@ -115,29 +116,28 @@ void mspi_dev_cfg(void) {
   QUADSPI->CR |= 16 << QUADSPI_CR_PRESCALER_Pos;
 }
 
-void mspi_interface_cleanup(void){
+void mspi_interface_cleanup(void) {
   // cleanup of functional registers
-  QUADSPI->CR   &= ~( QUADSPI_CR_EN );
-  QUADSPI->CCR  &= ~( QUADSPI_CCR_FMODE |
-                      QUADSPI_CCR_IMODE |
-                      QUADSPI_CCR_ADMODE|
-                      QUADSPI_CCR_DMODE |
-                      QUADSPI_CCR_ADSIZE|
-                      QUADSPI_CCR_INSTRUCTION );
+  QUADSPI->CR &= ~(QUADSPI_CR_EN);
+  QUADSPI->CCR &=
+      ~(QUADSPI_CCR_FMODE | QUADSPI_CCR_IMODE | QUADSPI_CCR_ADMODE |
+        QUADSPI_CCR_DMODE | QUADSPI_CCR_ADSIZE | QUADSPI_CCR_INSTRUCTION);
 
-  QUADSPI->DLR  &= ~(QUADSPI_DLR_DL);
-  QUADSPI->AR   &= ~(QUADSPI_AR_ADDRESS_Msk);
-  QUADSPI->DR   &= ~(QUADSPI_DR_DATA_Msk);
+  QUADSPI->DLR &= ~(QUADSPI_DLR_DL);
+  QUADSPI->AR &= ~(QUADSPI_AR_ADDRESS_Msk);
+  QUADSPI->DR &= ~(QUADSPI_DR_DATA_Msk);
 }
 
-u16 mspi_interface_wait_busy(void){
-  while ( QUADSPI->SR & QUADSPI_SR_BUSY ) {};
+u16 mspi_interface_wait_busy(void) {
+  while (QUADSPI->SR & QUADSPI_SR_BUSY) {
+  };
   // PENDING add timeout
   return EXIT_SUCCESS;
 }
 
 // TODO (add interface specifier f.e. OCTOSPI1/OCTOSPI2)
-u16 mspi_transfer_dma( struct mspi_cmd (*device_fun_handler)(void*),struct mspi_interface interface,void* argument){
+u16 mspi_transfer_dma(struct mspi_cmd (*device_fun_handler)(void *),
+                      struct mspi_interface interface, void *argument) {
 
   struct mspi_cmd cmd = {0};
   cmd = device_fun_handler(argument);
@@ -145,54 +145,58 @@ u16 mspi_transfer_dma( struct mspi_cmd (*device_fun_handler)(void*),struct mspi_
   // PENDING save interface registers for check
   mspi_interface_cleanup();
   // set functional mode
-  QUADSPI->CCR |=  ( ( cmd.fun_mode << QUADSPI_CCR_FMODE_Pos ) );
+  QUADSPI->CCR |= ((cmd.fun_mode << QUADSPI_CCR_FMODE_Pos));
   // set *-MODES (number of transfer lines)
-  QUADSPI->CCR |=  ( ( cmd.instr_mode << QUADSPI_CCR_IMODE_Pos )   |
-                     ( cmd.addr_mode  << QUADSPI_CCR_ADMODE_Pos )  | 
-                     ( cmd.data_mode  << QUADSPI_CCR_DMODE_Pos ) );
+  QUADSPI->CCR |= ((cmd.instr_mode << QUADSPI_CCR_IMODE_Pos) |
+                   (cmd.addr_mode << QUADSPI_CCR_ADMODE_Pos) |
+                   (cmd.data_mode << QUADSPI_CCR_DMODE_Pos));
   // set length of *-PHASES (if in use)
-  if (cmd.addr_mode > 0 ){
-    QUADSPI->CCR  |=  ( cmd.addr_size <<  QUADSPI_CCR_ADSIZE_Pos );
+  if (cmd.addr_mode > 0) {
+    QUADSPI->CCR |= (cmd.addr_size << QUADSPI_CCR_ADSIZE_Pos);
   }
-  if (cmd.data_mode > 0){
-    QUADSPI->DLR  |=  ( cmd.data_size << QUADSPI_DLR_DL_Pos);
+  if (cmd.data_mode > 0) {
+    QUADSPI->DLR |= (cmd.data_size << QUADSPI_DLR_DL_Pos);
   }
-  //enable the peripheral
-  QUADSPI->CR  |=  ( QUADSPI_CR_EN );
-  //set intruction, address (if in use)
-  QUADSPI->CCR |=  ( cmd.instr_cmd << QUADSPI_CCR_INSTRUCTION_Pos );
-  // if read functional mode enable the dma pull channel before sending the address
-  if (cmd.data_mode > 0){
-    if(cmd.fun_mode == 0b01){ //read -- dma pull
-      //enable dma channel
+  // enable the peripheral
+  QUADSPI->CR |= (QUADSPI_CR_EN);
+  // set intruction, address (if in use)
+  QUADSPI->CCR |= (cmd.instr_cmd << QUADSPI_CCR_INSTRUCTION_Pos);
+  // if read functional mode enable the dma pull channel before sending the
+  // address
+  if (cmd.data_mode > 0) {
+    if (cmd.fun_mode == 0b01) { // read -- dma pull
+      // enable dma channel
     }
   }
   // Set the address if needed by the command
-  if (cmd.addr_mode > 0 ){
-    QUADSPI->AR  |=  ( cmd.addr_cmd << QUADSPI_AR_ADDRESS_Pos); //for now even plane, beginning of page
+  if (cmd.addr_mode > 0) {
+    QUADSPI->AR |=
+        (cmd.addr_cmd
+         << QUADSPI_AR_ADDRESS_Pos); // for now even plane, beginning of page
   }
 
-  // if read functional mode enable the dma pull channel before sending the address
-  if (cmd.data_mode > 0){
-    if(cmd.fun_mode == 0b00){ //write -- dma push
-      //enable dma channel
+  // if read functional mode enable the dma pull channel before sending the
+  // address
+  if (cmd.data_mode > 0) {
+    if (cmd.fun_mode == 0b00) { // write -- dma push
+      // enable dma channel
     }
   }
-  
-  //wait for the transaction to complete (+ timeout and abort)
-  if ( mspi_interface_wait_busy()) {
-    QUADSPI->CR  &= ~( QUADSPI_CR_EN ); // disable the interface in anay case
+
+  // wait for the transaction to complete (+ timeout and abort)
+  if (mspi_interface_wait_busy()) {
+    QUADSPI->CR &= ~(QUADSPI_CR_EN); // disable the interface in anay case
     return ERROR_MSPI_INTERFACE_STUCK;
   }
-  
-  QUADSPI->CR  &= ~( QUADSPI_CR_EN );
+
+  QUADSPI->CR &= ~(QUADSPI_CR_EN);
 
   return EXIT_SUCCESS;
 }
 
-u16 mspi_autopoll_wait( struct mspi_cmd (*device_fun_handler)(void*),
-                         struct mspi_interface interface, void* argument,
-                         u32 mask, u32 match){
+u16 mspi_autopoll_wait(struct mspi_cmd (*device_fun_handler)(void *),
+                       struct mspi_interface interface, void *argument,
+                       u32 mask, u32 match) {
 
   struct mspi_cmd cmd = {0};
   cmd = device_fun_handler(argument);
@@ -200,61 +204,65 @@ u16 mspi_autopoll_wait( struct mspi_cmd (*device_fun_handler)(void*),
   // Set the 'mask', 'match', and 'polling interval' values.
   QUADSPI->PSMKR = mask;
   QUADSPI->PSMAR = match;
-  QUADSPI->PIR   = 0x10;
+  QUADSPI->PIR = 0x10;
 
   // PENDING save interface registers for check
   mspi_interface_cleanup();
 
   // set functional mode
-  QUADSPI->CCR |=  ( ( cmd.fun_mode << QUADSPI_CCR_FMODE_Pos ) );
+  QUADSPI->CCR |= ((cmd.fun_mode << QUADSPI_CCR_FMODE_Pos));
   // set *-MODES (number of transfer lines)
-  QUADSPI->CCR |=  ( ( cmd.instr_mode << QUADSPI_CCR_IMODE_Pos )   |
-                     ( cmd.addr_mode  << QUADSPI_CCR_ADMODE_Pos )  | 
-                     ( cmd.data_mode  << QUADSPI_CCR_DMODE_Pos ) );
+  QUADSPI->CCR |= ((cmd.instr_mode << QUADSPI_CCR_IMODE_Pos) |
+                   (cmd.addr_mode << QUADSPI_CCR_ADMODE_Pos) |
+                   (cmd.data_mode << QUADSPI_CCR_DMODE_Pos));
 
   // set length of *-PHASES (if in use)
-  if (cmd.addr_mode > 0 ){
-    QUADSPI->CCR  |=  ( cmd.addr_size <<  QUADSPI_CCR_ADSIZE_Pos );
+  if (cmd.addr_mode > 0) {
+    QUADSPI->CCR |= (cmd.addr_size << QUADSPI_CCR_ADSIZE_Pos);
   }
-  if (cmd.data_mode > 0){
-    QUADSPI->DLR  |=  ( cmd.data_size << QUADSPI_DLR_DL_Pos);
+  if (cmd.data_mode > 0) {
+    QUADSPI->DLR |= (cmd.data_size << QUADSPI_DLR_DL_Pos);
   }
-  //enable the peripheral
-  QUADSPI->CR  |=  ( QUADSPI_CR_EN );
-  //set intruction, address (if in use)
-  QUADSPI->CCR |=  ( cmd.instr_cmd << QUADSPI_CCR_INSTRUCTION_Pos );
-  // if read functional mode enable the dma pull channel before sending the address
-  if (cmd.data_mode > 0){
-    if(cmd.fun_mode == 0b01){ //read -- dma pull
-      //enable dma channel
+  // enable the peripheral
+  QUADSPI->CR |= (QUADSPI_CR_EN);
+  // set intruction, address (if in use)
+  QUADSPI->CCR |= (cmd.instr_cmd << QUADSPI_CCR_INSTRUCTION_Pos);
+  // if read functional mode enable the dma pull channel before sending the
+  // address
+  if (cmd.data_mode > 0) {
+    if (cmd.fun_mode == 0b01) { // read -- dma pull
+      // enable dma channel
     }
   }
   // Set the address if needed by the command
-  if (cmd.addr_mode > 0 ){
-    QUADSPI->AR  |=  ( cmd.addr_cmd << QUADSPI_AR_ADDRESS_Pos); //for now even plane, beginning of page
+  if (cmd.addr_mode > 0) {
+    QUADSPI->AR |=
+        (cmd.addr_cmd
+         << QUADSPI_AR_ADDRESS_Pos); // for now even plane, beginning of page
   }
 
-  // if read functional mode enable the dma pull channel before sending the address
-  if (cmd.data_mode > 0){
-    if(cmd.fun_mode == 0b00){ //write -- dma push
-      //enable dma channel
+  // if read functional mode enable the dma pull channel before sending the
+  // address
+  if (cmd.data_mode > 0) {
+    if (cmd.fun_mode == 0b00) { // write -- dma push
+      // enable dma channel
     }
   }
-  
-  //wait for the transaction to complete (+ timeout and abort)
-  if ( mspi_interface_wait_busy()) {
-    QUADSPI->CR  &= ~( QUADSPI_CR_EN ); // disable the interface in anay case
+
+  // wait for the transaction to complete (+ timeout and abort)
+  if (mspi_interface_wait_busy()) {
+    QUADSPI->CR &= ~(QUADSPI_CR_EN); // disable the interface in anay case
     return ERROR_MSPI_INTERFACE_STUCK;
   }
 
   // Wait for a match.
-  while ( QUADSPI->SR & QUADSPI_SR_BUSY ) {};
+  while (QUADSPI->SR & QUADSPI_SR_BUSY) {
+  };
   // Acknowledge the 'status match flag.'
-  QUADSPI->FCR |=  ( QUADSPI_FCR_CSMF );
+  QUADSPI->FCR |= (QUADSPI_FCR_CSMF);
   // Un-set the data mode and disable auto-polling.
-  QUADSPI->CCR  &= ~( QUADSPI_CCR_FMODE |
-                      QUADSPI_CCR_DMODE );
+  QUADSPI->CCR &= ~(QUADSPI_CCR_FMODE | QUADSPI_CCR_DMODE);
   // Disable the peripheral.
-  QUADSPI->CR  &= ~( QUADSPI_CR_EN );
+  QUADSPI->CR &= ~(QUADSPI_CR_EN);
   return EXIT_SUCCESS;
 }
--- hal/stm32l4r9_module_mspi_mt29.c
+++ hal/stm32l4r9_module_mspi_mt29.c
@@ -1,55 +1,55 @@
-#include "module_mspi_mt29.h"
+#include "stm32l4r9_module_mspi_mt29.h"
 
 #define IS_DUAL_MEM 0U;
 
 /**
-* --------------------------------------------------------------------------- *
-*       DEVICE SPECIFIC COMMAND FUNCTIONS
-* --------------------------------------------------------------------------- *
-*/
+ * --------------------------------------------------------------------------- *
+ *       DEVICE SPECIFIC COMMAND FUNCTIONS
+ * --------------------------------------------------------------------------- *
+ */
 
-struct mspi_cmd mspi_device_write_unlock(void*);
-struct mspi_cmd mspi_device_write_lock(void*);
-struct mspi_cmd mspi_device_write_enable(void*);
-struct mspi_cmd mspi_device_get_status(void*);
-struct mspi_cmd mspi_device_page_read_from_nand(void*);
-struct mspi_cmd mspi_device_page_read_from_cache_SINGLE(void*);
-struct mspi_cmd mspi_device_page_read_from_cache_QUAD(void*);
-struct mspi_cmd mspi_device_page_load_SINGLE(void*);
-struct mspi_cmd mspi_device_page_load_QUAD(void*);
-struct mspi_cmd mspi_device_page_program(void*);
-struct mspi_cmd mspi_device_block_erase(void*);
+struct mspi_cmd mspi_device_write_unlock(void *);
+struct mspi_cmd mspi_device_write_lock(void *);
+struct mspi_cmd mspi_device_write_enable(void *);
+struct mspi_cmd mspi_device_get_status(void *);
+struct mspi_cmd mspi_device_page_read_from_nand(void *);
+struct mspi_cmd mspi_device_page_read_from_cache_SINGLE(void *);
+struct mspi_cmd mspi_device_page_read_from_cache_QUAD(void *);
+struct mspi_cmd mspi_device_page_load_SINGLE(void *);
+struct mspi_cmd mspi_device_page_load_QUAD(void *);
+struct mspi_cmd mspi_device_page_program(void *);
+struct mspi_cmd mspi_device_block_erase(void *);
 
-struct mspi_cmd mspi_device_wait_write_enable(void*);
-//struct mspi_cmd mspi_device_wait_program_complete(void);
+struct mspi_cmd mspi_device_wait_write_enable(void *);
+// struct mspi_cmd mspi_device_wait_program_complete(void);
 /**
-* --------------------------------------------------------------------------- *
-*       ADDRESS MANAGEMENT LOCAL FUNCTIONS
-* --------------------------------------------------------------------------- *
-*/
+ * --------------------------------------------------------------------------- *
+ *       ADDRESS MANAGEMENT LOCAL FUNCTIONS
+ * --------------------------------------------------------------------------- *
+ */
 
-u32 RA_gen(struct nand_addr nand_addr){
+u32 RA_gen(struct nand_addr nand_addr) {
   u32 RA = {0};
-  RA |= ( nand_addr.block << 17);
+  RA |= (nand_addr.block << 17);
   RA |= (nand_addr.page << 11);
   RA >>= 8;
   return RA;
 }
 
-u32 CA_gen(struct nand_addr nand_addr){
+u32 CA_gen(struct nand_addr nand_addr) {
   u32 CA = {0};
-  CA |= ( (nand_addr.block & 0x01) << 12);
-  CA |= ( nand_addr.column );
+  CA |= ((nand_addr.block & 0x01) << 12);
+  CA |= (nand_addr.column);
   return CA;
 }
 
 /**
-* --------------------------------------------------------------------------- *
-*       COMMAND DEFINITION
-* --------------------------------------------------------------------------- *
-*/
+ * --------------------------------------------------------------------------- *
+ *       COMMAND DEFINITION
+ * --------------------------------------------------------------------------- *
+ */
 
-//TODO: reduce the scope of these enums only to this file
+// TODO: reduce the scope of these enums only to this file
 enum fun_modes {
   WRITE = 0,
   READ,
@@ -72,7 +72,7 @@ enum addr_size {
   _32_bit,
 };
 
-struct mspi_cmd mspi_device_write_unlock(void* placeholder){
+struct mspi_cmd mspi_device_write_unlock(void *placeholder) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -91,7 +91,7 @@ struct mspi_cmd mspi_device_write_unlock(void* placeholder){
   return cmd;
 }
 
-struct mspi_cmd mspi_device_write_lock(void* placeholder){
+struct mspi_cmd mspi_device_write_lock(void *placeholder) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -110,7 +110,7 @@ struct mspi_cmd mspi_device_write_lock(void* placeholder){
   return cmd;
 }
 
-struct mspi_cmd mspi_device_write_enable(void* placeholder){
+struct mspi_cmd mspi_device_write_enable(void *placeholder) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -121,7 +121,7 @@ struct mspi_cmd mspi_device_write_enable(void* placeholder){
   return cmd;
 }
 
-struct mspi_cmd mspi_device_get_status(void* placeholder){
+struct mspi_cmd mspi_device_get_status(void *placeholder) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = READ;
@@ -139,7 +139,7 @@ struct mspi_cmd mspi_device_get_status(void* placeholder){
   return cmd;
 }
 
-struct mspi_cmd mspi_device_page_read_from_nand(void* nand_addr){
+struct mspi_cmd mspi_device_page_read_from_nand(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -150,14 +150,14 @@ struct mspi_cmd mspi_device_page_read_from_nand(void* nand_addr){
   cmd.addr_mode = SINGLE;
   cmd.addr_size = _24_bit;
 
-  cmd.addr_cmd = RA_gen(*(struct nand_addr*) nand_addr);
+  cmd.addr_cmd = RA_gen(*(struct nand_addr *)nand_addr);
 
   cmd.data_mode = NP;
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_page_read_from_cache_SINGLE(void* nand_addr){
+struct mspi_cmd mspi_device_page_read_from_cache_SINGLE(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = READ;
@@ -167,15 +167,15 @@ struct mspi_cmd mspi_device_page_read_from_cache_SINGLE(void* nand_addr){
 
   cmd.addr_mode = SINGLE;
   cmd.addr_size = _16_bit;
-  cmd.addr_cmd = CA_gen(* (struct nand_addr*)nand_addr);
+  cmd.addr_cmd = CA_gen(*(struct nand_addr *)nand_addr);
 
   cmd.data_mode = SINGLE;
-  cmd.data_size = MT29_PAGE_SIZE-1;
+  cmd.data_size = MT29_PAGE_SIZE - 1;
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_page_read_from_cache_QUAD(void* nand_addr){
+struct mspi_cmd mspi_device_page_read_from_cache_QUAD(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = READ;
@@ -185,15 +185,15 @@ struct mspi_cmd mspi_device_page_read_from_cache_QUAD(void* nand_addr){
 
   cmd.addr_mode = QUAD;
   cmd.addr_size = _16_bit;
-  cmd.addr_cmd = CA_gen(*(struct nand_addr*)nand_addr);
+  cmd.addr_cmd = CA_gen(*(struct nand_addr *)nand_addr);
 
   cmd.data_mode = QUAD;
-  cmd.data_size = MT29_PAGE_SIZE-1;
+  cmd.data_size = MT29_PAGE_SIZE - 1;
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_page_load_SINGLE(void* nand_addr){
+struct mspi_cmd mspi_device_page_load_SINGLE(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -203,15 +203,15 @@ struct mspi_cmd mspi_device_page_load_SINGLE(void* nand_addr){
 
   cmd.addr_mode = SINGLE;
   cmd.addr_size = _16_bit;
-  cmd.addr_cmd = CA_gen(*(struct nand_addr*) nand_addr);
+  cmd.addr_cmd = CA_gen(*(struct nand_addr *)nand_addr);
 
   cmd.data_mode = SINGLE;
-  cmd.data_size = MT29_PAGE_SIZE-1;
+  cmd.data_size = MT29_PAGE_SIZE - 1;
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_page_load_QUAD(void* nand_addr){
+struct mspi_cmd mspi_device_page_load_QUAD(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -221,15 +221,15 @@ struct mspi_cmd mspi_device_page_load_QUAD(void* nand_addr){
 
   cmd.addr_mode = SINGLE;
   cmd.addr_size = _16_bit;
-  cmd.addr_cmd = CA_gen(*(struct nand_addr*) nand_addr);
+  cmd.addr_cmd = CA_gen(*(struct nand_addr *)nand_addr);
 
   cmd.data_mode = QUAD;
-  cmd.data_size = MT29_PAGE_SIZE-1;
+  cmd.data_size = MT29_PAGE_SIZE - 1;
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_page_program(void* nand_addr){
+struct mspi_cmd mspi_device_page_program(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -239,12 +239,12 @@ struct mspi_cmd mspi_device_page_program(void* nand_addr){
 
   cmd.addr_mode = SINGLE;
   cmd.addr_size = _24_bit;
-  cmd.addr_cmd = RA_gen(*(struct nand_addr*) nand_addr);
+  cmd.addr_cmd = RA_gen(*(struct nand_addr *)nand_addr);
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_block_erase(void* nand_addr) {
+struct mspi_cmd mspi_device_block_erase(void *nand_addr) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = WRITE;
@@ -254,12 +254,12 @@ struct mspi_cmd mspi_device_block_erase(void* nand_addr) {
 
   cmd.addr_mode = SINGLE;
   cmd.addr_size = _24_bit;
-  cmd.addr_cmd = RA_gen(*(struct nand_addr*) nand_addr);
+  cmd.addr_cmd = RA_gen(*(struct nand_addr *)nand_addr);
 
   return cmd;
 }
 
-struct mspi_cmd mspi_device_wait_write_enable(void*){
+struct mspi_cmd mspi_device_wait_write_enable(void *) {
   struct mspi_cmd cmd = {0};
   cmd.is_double_mem = IS_DUAL_MEM;
   cmd.fun_mode = AUTOPOLLING;
@@ -277,29 +277,28 @@ struct mspi_cmd mspi_device_wait_write_enable(void*){
   return cmd;
 }
 
-
 /**
-* --------------------------------------------------------------------------- *
-*       DEVICE OBJECT CONSTRUCTOR
-* --------------------------------------------------------------------------- *
-*/
+ * --------------------------------------------------------------------------- *
+ *       DEVICE OBJECT CONSTRUCTOR
+ * --------------------------------------------------------------------------- *
+ */
 
-struct mspi_device mspi_device_constr(void){
+struct mspi_device mspi_device_constr(void) {
   struct mspi_device dev = {0};
-  dev.write_unlock                = &mspi_device_write_unlock;
-  dev.write_lock                  = &mspi_device_write_lock;
-  dev.write_enable                = &mspi_device_write_enable;
-  dev.get_status                  = &mspi_device_get_status;
-  dev.page_read_from_nand         = &mspi_device_page_read_from_nand;
+  dev.write_unlock = &mspi_device_write_unlock;
+  dev.write_lock = &mspi_device_write_lock;
+  dev.write_enable = &mspi_device_write_enable;
+  dev.get_status = &mspi_device_get_status;
+  dev.page_read_from_nand = &mspi_device_page_read_from_nand;
   dev.page_read_from_cache_SINGLE = &mspi_device_page_read_from_cache_SINGLE;
-  dev.page_read_from_cache_QUAD   = &mspi_device_page_read_from_cache_QUAD;
-  dev.page_load_SINGLE            = &mspi_device_page_load_SINGLE;
-  dev.page_load_QUAD              = &mspi_device_page_load_QUAD;
-  dev.page_program                = &mspi_device_page_program;
-  dev.block_erase                 = &mspi_device_block_erase;
+  dev.page_read_from_cache_QUAD = &mspi_device_page_read_from_cache_QUAD;
+  dev.page_load_SINGLE = &mspi_device_page_load_SINGLE;
+  dev.page_load_QUAD = &mspi_device_page_load_QUAD;
+  dev.page_program = &mspi_device_page_program;
+  dev.block_erase = &mspi_device_block_erase;
 
-  dev.write_enable                = &mspi_device_write_enable; //TODO implement
-  //dev.wait_program_complete       = &mspi_device_wait_program_complete;
+  dev.write_enable = &mspi_device_write_enable; // TODO implement
+  // dev.wait_program_complete       = &mspi_device_wait_program_complete;
 
   return dev;
 }
