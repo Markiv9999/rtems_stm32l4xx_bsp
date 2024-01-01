cd410dbd4076b327c197c73240338074171ecfb8
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2024-01-01 12:29:59 +0000

    fix

--- include/ext_error_codes.h
+++ include/ext_error_codes.h
@@ -2,7 +2,6 @@
 // manner
 #define ERROR_GENERIC 1U // to remove
 
-#define EXIT_SUCCESS 0U
 #define EXIT_UNDEFINED 0U
 #define ERROR_MSPI_WEL 1001U // a bit unclear
 #define ERROR_MSPI_INTERFACE_STUCK 10010U
