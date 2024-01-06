85c5073f66b7a2004f6a9bc7f95a9a893f3096b2
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2024-01-06 17:32:47 +0000

    fix

--- hal/stm32l4r9_module_mspi.c
+++ hal/stm32l4r9_module_mspi.c
@@ -8,8 +8,8 @@
  *       INTERFACE SPECIFIC METHODS
  * --------------------------------------------------------------------------- *
  */
-extern char dma_push_buffer;
-extern char dma_pull_buffer;
+extern char dma_push_buffer[MT29_PAGE_SIZE];
+extern char dma_pull_buffer[MT29_PAGE_SIZE];
 
 int mspi_init(void) {
   /* this procedure satisfy the following functions:
