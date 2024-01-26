55ae0906310924d34c47a5acd095342a8be4e1ee
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2024-01-26 18:10:22 +0000

    fix

--- modules/hwlist_agent.c
+++ modules/hwlist_agent.c
@@ -5,7 +5,7 @@ void hwlist_init(void) {
   hwlist_init_node_pool();
 }
 
-void hwlist_require(struct Node *head, u32 (*child_if)(void),
+void hwlist_require(struct Node **head, u32 (*child_if)(void),
                     void (*father_if)(void)) {
 
   struct node_data c_data;
@@ -15,12 +15,12 @@ void hwlist_require(struct Node *head, u32 (*child_if)(void),
 
   /* if father id is null, then add at the beginning of the LL */
   if (!father_if) {
-    hwlist_insert_beginning(&head, c_data);
+    hwlist_insert_beginning(head, c_data);
   } else {
     /* if father is not null, add afer father node */
 
     struct Node *f_node_ptr;
-    f_node_ptr = hwlist_search_frm_hwif(head, f_data);
+    f_node_ptr = hwlist_search_frm_hwif(*head, f_data);
     hwlist_insert_after_node(f_node_ptr, c_data);
   }
 }
