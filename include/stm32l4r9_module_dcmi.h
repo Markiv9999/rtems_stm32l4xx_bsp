f3a9345367126da5e13691b08fa3d7e83369d6ee
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2024-02-04 17:06:31 +0000

    improved capture buffer, workig

--- include/stm32l4r9_module_dcmi.h
+++ include/stm32l4r9_module_dcmi.h
@@ -2,11 +2,12 @@
 #include "ext_error_codes.h"
 #include "ext_typedefs.h"
 #include "stm32l4r9_module_i2c.h"
+#include "stm32l4r9_module_mspi_mt29.h"
 #include "stm32l4r9_module_ov5640.h"
 #include "stm32l4r9xx.h"
 
-#define MAX_DMA_TRS_SIZE 65535     // what it should be
-#define IMG_METADATA_MAX_WSIZE 130 // what is should be
+#define MAX_DMA_TRS_SIZE 65535                  // what it should be
+#define IMG_METADATA_MAX_WSIZE MT29_PAGE_W_SIZE // what is should be
 
 /* External interfaces */
 void ov5640_configure_jpeg_720p(void);    // NOTE: WORKING
